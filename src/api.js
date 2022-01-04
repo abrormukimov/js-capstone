5645562f788d7a26765b3ff6ecbc0043afbaa94c
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-04 11:39:18 +0100

    Add post and get for reservations

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
-const url = 'https://api.tvmaze.com/shows';
-const key = 'CGPm9CVfAjvE2W0viDdC';
-const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const url = "https://api.tvmaze.com/shows";
+const key = "CGPm9CVfAjvE2W0viDdC";
+const baseUrl =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
 
 const getMovies = async () => {
   const response = await fetch(url);
@@ -8,19 +9,34 @@ const getMovies = async () => {
 };
 
 const postComments = async (itemId, username, comment) => {
-  const response = await fetch(`${baseUrl}${key}/comments`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        item_id: itemId,
-        username,
-        comment,
-      }),
-    });
+  const response = await fetch(`${baseUrl}${key}/comments`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      item_id: itemId,
+      username,
+      comment,
+    }),
+  });
+  return response.text();
+};
+const postReservations = async (itemId, username, date_start, date_end) => {
+  const response = await fetch(`${baseUrl}${key}/reservations`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      item_id: itemId,
+      username,
+      date_start,
+      date_end,
+    }),
+  });
   return response.text();
 };
 
@@ -28,9 +44,11 @@ const getComments = async (itemid) => {
   const response = await fetch(`${baseUrl}${key}/comments?item_id=${itemid}`);
   return response.json();
 };
-
-export {
-  getMovies,
-  postComments,
-  getComments,
+const getRes = async (itemid) => {
+  const response = await fetch(
+    `${baseUrl}${key}/reservations?item_id=${itemid}`
+  );
+  return response.json();
 };
+
+export { getMovies, postComments, getComments, getRes, postReservations };
--- src/comments.js
+++ src/comments.js
@@ -1,7 +1,7 @@
-import { getComments } from './api.js';
+import { getComments, getRes } from "./api.js";
 
 const display = async (commentsSection, id) => {
-  commentsSection.innerHTML = '';
+  commentsSection.innerHTML = "";
   const data = await getComments(id);
   const myList = data.map((item) => {
     const listItems = `<p>${item.username}: ${item.comment}</p>`;
@@ -10,6 +10,14 @@ const display = async (commentsSection, id) => {
   commentsSection.innerHTML = myList;
 };
 
-export {
-  display,
-}
+const displayRes = async (reserveSection, id) => {
+  reserveSection.innerHTML = "";
+  const data = await getRes(id);
+  const myList = data.map((item) => {
+    const listItems = `<p>${item.username}: ${item.date_start}: ${item.date_end}</p>`;
+    return listItems;
+  });
+  reserveSection.innerHTML = myList;
+};
+
+export { display, displayRes };
