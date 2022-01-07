3164883e338ce8b30bbb595a1d4a96b1b87c4bfc
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-07 18:18:49 +0100

    Add counter test

---
+++ src/commentCounter.test.js
@@ -0,0 +1,29 @@
+import { commentCounter } from './counter.js';
+const array = [{
+  creation_date: '21-12-2021',
+  username: 'Mike',
+  comment: 'Hi there',
+},
+{
+  creation_date: '21-12-2021',
+  username: 'Mike',
+  comment: 'Hi there',
+},
+{
+  creation_date: '21-12-2021',
+  username: 'Mike',
+  comment: 'Hi there',
+},
+];
+global.fetch = jest.fn(() => Promise.resolve({
+  status: 200,
+  json() {
+    return array;
+  },
+}));
+describe('Comments Count', () => {
+  test('the number of comments', async () => {
+    const result = await commentCounter('itemId');
+    expect(result).toBe(3);
+  });
+});
